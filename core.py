dc2a7970dc72fdc015730b271fbc1e17e5d5c12a
Rodrigo Carneiro <rodrigo_carneiro@ymail.com>
2018-09-06 19:08:42 +0100

    (WIP) First model

--- core.py
+++ core.py
@@ -1,4 +1,34 @@
-class Pype:
+import asyncio
+from random import random
 
-    def run(self):
-        return "[|===|]"
+class Pype:
+    async def execute(self, data):
+        #import ipdb; ipdb.set_trace()
+        if (self.conduits()):
+            data = await self.run(data)
+            asyncio.ensure_future(*(pype().execute(data) for pype in self.conduits()))
+        else:
+            await self.run(data)
+
+    def conduits(self):
+        return ()
+
+class PypeIn(Pype):
+    async def run(self, data):
+        print("#In: " , id(self), data)
+
+    def conduits(self):
+        return (PypeOut,)
+
+class PypeOut(Pype):
+    async def run(self, data):
+        await asyncio.sleep(random() * 3)
+        print("#Out: " , id(self), data)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.gather(*(PypeIn().execute({"data": "data"}) for _ in range(1, 10))))
+    pending_tasks = [
+        task for task in asyncio.Task.all_tasks() if not task.done()]
+    loop.run_until_complete(asyncio.gather(*pending_tasks))
+    loop.close()
